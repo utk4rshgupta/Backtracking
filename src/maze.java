9e68097f88577ae08e3c7c3bfcf3e010f238e04e
utk4rshgupta <utkarshgupta2803@gmail.com>
2026-02-02 20:55:01 +0530

    Print path

--- src/maze.java
+++ src/maze.java
@@ -1,6 +1,7 @@
 public class maze {
     public static void main(String[] args) {
-        System.out.println(countmaze(3,3));
+        //System.out.println(countmaze(3,3));
+        path("",3,3);
     }
     static int countmaze(int r , int c){
         if(r==1 || c==1){
@@ -9,5 +10,13 @@ public class maze {
         int left = countmaze(r-1,c);
         int right = countmaze(r,c-1);
         return left + right;
+    }
+
+    static void path(String p,int c , int r){
+        if(c==1 && r==1){
+            System.out.println(p);
+        }
+        if(r>1) path(p+'D',r-1,c);
+        if(c>1) path(p+'R',r,c-1);
     }
 }
